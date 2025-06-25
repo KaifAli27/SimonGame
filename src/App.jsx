24042e712e1cd2ad9a5aca3e95ef2b1d7266d53c
KaifAli27 <kaifali@cmail.carleton.ca>
2025-06-25 01:24:48 -0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import { useState } from 'react'
+import SimonGame from './components/SimonGame'
+
+function App() {
+  
+  return <SimonGame/>;
+  
+}
+
+export default App
---
+++ src/components/SimonGame.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const SimonGame = () => {
+  return (
+    <div>Simon Game</div>
+  );
+};
+
+export default SimonGame;
